3f4a7236b7255f1f6ee7ab10e422a58756246a16
andre <andrey.liskov03@mail.ru>
2024-10-30 02:17:21 +0500

    added charms inspect

--- config.example.js
+++ config.example.js
@@ -8,16 +8,10 @@ module.exports = {
     // List of usernames and passwords for the Steam accounts
     'logins': [
         {
-            'user': 'USERNAME',
-            'pass': 'PASSWORD',
-            // You can either use a 2FA email/mobile token (5 letters/digits), or the shared_secret of mobile 2FA
-            'auth': '2FA_TOKEN'
+            'user': '123',
+            'pass': '123',
+            'auth': '123'
         },
-        {
-            'user': 'USERNAME_2',
-            'pass': 'PASSWORD_2',
-            'auth': '2FA_TOKEN_2'
-        }
     ],
     // Optional HTTP/SOCKS5 proxies to auto-rotate for each bot in a round-robin
     'proxies': [],
@@ -26,7 +20,7 @@ module.exports = {
         // Amount of attempts for each request to Valve
         'max_attempts': 1,
         // Amount of milliseconds to wait between subsequent requests to Valve (per bot)
-        'request_delay': 1100,
+        'request_delay': 700,
         // Amount of milliseconds to wait until a request to Valve is timed out
         'request_ttl': 2000,
         // OPTIONAL: Settings for Steam User (https://github.com/DoctorMcKay/node-steam-user#options-)
@@ -53,15 +47,15 @@ module.exports = {
     // Logging Level (error, warn, info, verbose, debug, silly)
     'logLevel': 'debug',
     // Max amount of simultaneous requests from the same IP  (incl. WS and HTTP/HTTPS), -1 for unlimited
-    'max_simultaneous_requests': 1,
+    'max_simultaneous_requests': -1,
     // Bool to enable game file updates from the SteamDB Github tracker (updated item definitions, images, names)
     'enable_game_file_updates': true,
     // Amount of seconds to wait between updating game files (0 = No Interval Updates)
     'game_files_update_interval': 3600,
     // Postgres connection string to store results in (ex. postgres://user:pass@127.0.0.1:5432/postgres?sslmode=disable)
-    'database_url': '',
+    'database_url': 'postgresql://postgres:visagemain0364@localhost:5432/cs2_fetch_cache',
     // OPTIONAL: Enable bulk inserts, may improve performance with many requests
-    'enable_bulk_inserts': false,
+    'enable_bulk_inserts': true,
     // OPTIONAL: Key by the caller to allow inserting price information, required to use the feature
     'price_key': '',
     // OPTIONAL: Key by the caller to allow placing bulk searches
